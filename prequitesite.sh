249fef8c7f9d460fed1c7cb9496109ec7b073863
mark <marknguyenanl@gmail.com>
2024-09-09 05:02:48 +0700

    up

--- prequitesite.sh
+++ prequitesite.sh
@@ -31,15 +31,15 @@ sudo apt install \
   flameshot \
   clang \
   gcc \
+  obs-studio \
   rsync -y
 
 apt update -y && apt install -y gpg sudo wget curl
 sudo install -dm 755 /etc/apt/keyrings
-wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
 echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
 sudo apt update
 sudo apt install -y mise
-
 
 miseSetting="[tools]
 node = '18.18.0'
