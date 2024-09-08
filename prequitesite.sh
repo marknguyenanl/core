db53d96ef0326fe25b8b2d80bf3adc8e6bccf95d
marknguyenanl <162300553+marknguyenanl@users.noreply.github.com>
2024-09-08 16:47:56 +0000

    Update prequitesite.sh

--- prequitesite.sh
+++ prequitesite.sh
@@ -33,8 +33,13 @@ sudo apt install \
   gcc \
   rsync -y
 
-echo 'eval "$(mise activate bash)"' >>~/.bashrc
-mise up
+apt update -y && apt install -y gpg sudo wget curl
+sudo install -dm 755 /etc/apt/keyrings
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt update
+sudo apt install -y mise
+
 
 miseSetting="[tools]
 node = "18.18.0"
@@ -115,3 +120,6 @@ python_compile = false"
 
 mkdir -p $HOME/.config/mise/
 echo "$miseSetting" >$HOME/.config/mise/config.toml
+
+echo 'eval "$(mise activate bash)"' >>~/.bashrc
+mise up
